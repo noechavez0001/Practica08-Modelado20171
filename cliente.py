2ad8cc3888d3f7e8a3ccd06dd05e2c32261d9f1c
Noe Chavez <noechavez0001@gmail.com>
2016-10-06 22:05:18 -0500

    interfaz grafica cliente y servidor terminada

---
+++ cliente.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python
+
+import sys
+from PyQt4 import QtCore, QtGui, uic
+
+class ImageDialog(QtGui.QDialog):
+     def __init__(self):
+        QtGui.QDialog.__init__(self)
+        # Set up the user interface from Designer.
+        self.ui = uic.loadUi("cliente.ui")
+        self.ui.tableWidget.horizontalHeader().hide()
+        self.ui.tableWidget.verticalHeader().hide()
+        self.ui.tableWidget.setRowCount(5)
+        self.ui.tableWidget.setColumnCount(5)
+        self.ui.tableWidget.horizontalHeader().setResizeMode(QtGui.QHeaderView.Stretch)
+        self.ui.tableWidget.verticalHeader().setResizeMode(QtGui.QHeaderView.Stretch)
+        self.ui.show()
+
+
+app = QtGui.QApplication(sys.argv)
+window = ImageDialog()
+sys.exit(app.exec_())
